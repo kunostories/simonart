6c65f05ab96417ec1cfba0de4dacadddd318d694
Shawn Roe <english@sroeco.com>
2014-09-29 21:38:00 +0900

    Webs index

---
+++ galleries/webs/index.php
@@ -0,0 +1,35 @@
+<? include("../../head.php"); ?>
+        <!-- The Gallery as lightbox dialog, should be a child element of the document body -->
+        <div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
+            <div class="slides"></div>
+            <h3 class="title"></h3>
+            <a class="prev">‹</a>
+            <a class="next">›</a>
+            <a class="close">×</a>
+            <a class="play-pause"></a>
+            <ol class="indicator"></ol>
+        </div>
+
+        <div id="links">
+          <p>Click any image to view gallery.</p>
+            <a href="23.jpg" title="Smile 9.13.2011.16:03:30, 2011, Dimensions Variable, Lacquer on Nephila Clavata Spider Web. 2011">
+                <img src="320/23.jpg" alt="Smile 9.13.2011.16:03:30, 2011, Dimensions Variable, Lacquer on Nephila Clavata Spider Web. 2011">
+            </a>
+            <a href="26.jpg" title="Skull 10.7.2012.6:17:26 P.M., Dimensions Variable, Lacquer on Nephila Clavata Spider Web. 2012">
+                <img src="320/26.jpg" alt="Skull 10.7.2012.6:17:26 P.M., Dimensions Variable, Lacquer on Nephila Clavata Spider Web. 2012">
+            </a>
+            <a href="29.jpg" title="Rainbow (View 2), Dimensions Variable, Lacquer on Nephila Clavata Spider Web. 2011">
+                <img src="320/29.jpg" alt="Rainbow (View 2), Dimensions Variable, Lacquer on Nephila Clavata Spider Web. 2011">
+            </a>
+            <a href="32.jpg" title="Red, Yellow, and Blue in Excited States (View 2, Version1), Dimensions Variable, Lacquer on Nephila Clavata Spider Web. 2011">
+                <img src="320/32.jpg" alt="Red, Yellow, and Blue in Excited States (View 2, Version1), Dimensions Variable, Lacquer on Nephila Clavata Spider Web. 2011">
+            </a>
+            <a href="34.jpg" title="Excited Yellow Fading to Black, Dimensions Variable, Lacquer on Nephila Clavata Spider Web. 2011">
+                <img src="320/34.jpg" alt="Excited Yellow Fading to Black, Dimensions Variable, Lacquer on Nephila Clavata Spider Web. 2011">
+            </a>
+            <a href="42.jpg" title="On Our Own; Together (View 3), 107cm x 107cm x 135cm, Lacquer on Nephila Clavata Spider Web. 2012">
+                <img src="320/42.jpg" alt="On Our Own; Together (View 3), 107cm x 107cm x 135cm, Lacquer on Nephila Clavata Spider Web. 2012">
+            </a>
+        </div>
+
+<? include("../../foot.php"); ?>
