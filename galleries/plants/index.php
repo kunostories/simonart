83dcebca795a4a251be001750fb8d02d33ccbb25
Shawn Roe <english@sroeco.com>
2014-09-29 21:37:38 +0900

    Proper Image titles

--- galleries/plants/index.php
+++ galleries/plants/index.php
@@ -12,35 +12,35 @@
 
         <div id="links">
           <p>Click any image to view gallery.</p>
-            <a href="10.jpg" title="web">
-                <img src="320/10.jpg" alt="web">
+            <a href="10.jpg" title="8.3.2010.10:51:10 A.M. Dimensions Variable. Acrylic on Tree. 2010">
+                <img src="320/10.jpg" alt="8.3.2010.10:51:10 A.M. Dimensions Variable. Acrylic on Tree. 2010">
             </a>
-            <a href="11.jpg" title="web">
-                <img src="320/11.jpg" alt="web">
+            <a href="11.jpg" title="8.3.2010.10:51:10 A.M. Detail">
+                <img src="320/11.jpg" alt="8.3.2010.10:51:10 A.M. Detail">
             </a>
-            <a href="12.jpg" title="web">
-                <img src="320/12.jpg" alt="web">
+            <a href="12.jpg" title="11.13.2010.2:42:11 P.M. Dimensions Variable. Lacquer on Tree. 2010">
+                <img src="320/12.jpg" alt="11.13.2010.2:42:11 P.M. Dimensions Variable. Lacquer on Tree. 2010">
             </a>
-            <a href="13.jpg" title="web">
-                <img src="320/13.jpg" alt="web">
+            <a href="13.jpg" title="2.11.2011.12:23:08 P.M. Dimensions Variable. Lacquer on Cactus. 2011">
+                <img src="320/13.jpg" alt="2.11.2011.12:23:08 P.M. Dimensions Variable. Lacquer on Cactus. 2011">
             </a>
-            <a href="14.jpg" title="web" data-gallery>
-                <img src="320/14.jpg" alt="web">
+            <a href="14.jpg" title="6.20.2011.6:42:41 P.M. Dimensions Variable. Lacquer on Tree. 2011" data-gallery>
+                <img src="320/14.jpg" alt="6.20.2011.6:42:41 P.M. Dimensions Variable. Lacquer on Tree. 2011">
             </a>
-            <a href="15.jpg" title="web" data-gallery>
-                <img src="320/15.jpg" alt="web">
+            <a href="15.jpg" title="10.17.2010.4:33:18 P.M. Dimensions Variable. Lacquer and Stickers on Tree. 2010" data-gallery>
+                <img src="320/15.jpg" alt="10.17.2010.4:33:18 P.M. Dimensions Variable. Lacquer and Stickers on Tree. 2010">
             </a>
-            <a href="16.jpg" title="web">
-                <img src="320/16.jpg" alt="web">
+            <a href="16.jpg" title="10.17.2010.4:33:18 P.M. Detail">
+                <img src="320/16.jpg" alt="10.17.2010.4:33:18 P.M. Detail">
             </a>
-            <a href="17.jpg" title="web">
-                <img src="320/17.jpg" alt="web">
+            <a href="17.jpg" title="10.3.2010.5:00:31 P.M. Dimensions Variable. Lacquer on Tree. 2010">
+                <img src="320/17.jpg" alt="10.3.2010.5:00:31 P.M. Dimensions Variable. Lacquer on Tree. 2010">
             </a>
-            <a href="18.jpg" title="web" data-gallery>
-                <img src="320/18.jpg" alt="web">
+            <a href="18.jpg" title="11.6.2010.3:50:16 P.M. Dimensions Variable. Lacquer on Tree. 2010" data-gallery>
+                <img src="320/18.jpg" alt="11.6.2010.3:50:16 P.M. Dimensions Variable. Lacquer on Tree. 2010">
             </a>
-            <a href="22.jpg" title="web" data-gallery>
-                <img src="320/22.jpg" alt="web">
+            <a href="22.jpg" title="1.7.2013.12:54:21 P.M. Dimensions Variable. Lacquer on Cactus. 2013" data-gallery>
+                <img src="320/22.jpg" alt="1.7.2013.12:54:21 P.M. Dimensions Variable. Lacquer on Cactus. 2013">
             </a>
         </div>
 
--- galleries/silence/index.php
+++ galleries/silence/index.php
@@ -12,23 +12,23 @@
 
         <div id="links">
           <p>Click any image to view gallery.</p>
-            <a href="1.jpg" title="web">
-                <img src="320/1.jpg" alt="web">
+            <a href="1.jpg" title="Self-Titled (English/French) 174cm x 98cm. Acrylic and Lacquer on Canvas. 2011">
+                <img src="320/1.jpg" alt="Self-Titled (English/French) 174cm x 98cm. Acrylic and Lacquer on Canvas. 2011">
             </a>
-            <a href="3.jpg" title="web">
-                <img src="320/3.jpg" alt="web">
+            <a href="3.jpg" title="Self-Titled (Silence – Chinese/Japanese) 189cm x 94cm. Acrylic and Lacquer on Canvas. 2011">
+                <img src="320/3.jpg" alt="Self-Titled (Silence – Chinese/Japanese) 189cm x 94cm. Acrylic and Lacquer on Canvas. 2011">
             </a>
-            <a href="6.jpg" title="web">
-                <img src="320/6.jpg" alt="web">
+            <a href="6.jpg" title="Self-Titled (Silence – Arabic) 165cm x 77cm. Acrylic and Lacquer on Canvas. 2011">
+                <img src="320/6.jpg" alt="Self-Titled (Silence – Arabic) 165cm x 77cm. Acrylic and Lacquer on Canvas. 2011">
             </a>
-            <a href="8.jpg" title="web">
-                <img src="320/8.jpg" alt="web">
+            <a href="8.jpg" title="Self-Titled (Silence – Spanish/Portuguese) 165cm x 100cm. Acrylic and Lacquer on Canvas. 2012">
+                <img src="320/8.jpg" alt="Self-Titled (Silence – Spanish/Portuguese) 165cm x 100cm. Acrylic and Lacquer on Canvas. 2012">
             </a>
-            <a href="9.jpg" title="web" data-gallery>
-                <img src="320/9.jpg" alt="web">
+            <a href="9.jpg" title="Self-Titled (Silence – Thai) 180cm x 110cm. Acrylic and Lacquer on Canvas. 2012" data-gallery>
+                <img src="320/9.jpg" alt="Self-Titled (Silence – Thai) 180cm x 110cm. Acrylic and Lacquer on Canvas. 2012">
             </a>
-            <a href="100.jpg" title="web" data-gallery>
-                <img src="320/100.jpg" alt="web">
+            <a href="100.jpg" title="Installation View 1" data-gallery>
+                <img src="320/100.jpg" alt="Installation View 1">
             </a>
         </div>
 
