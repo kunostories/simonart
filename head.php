f86c80841fd9e6527b1bcb88ebd479bbd9acb412
Shawn Roe <english@sroeco.com>
2014-10-07 19:36:02 +0900

    Custom.less file

--- head.php
+++ head.php
@@ -16,9 +16,6 @@
     <link rel="stylesheet" href="http://blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
     <link rel="stylesheet" href="/simonart/include/css/bootstrap-image-gallery.min.css">
 
-    <!-- Custom styles for this template -->
-    <link href="/simonart/include/css/simonart.css" rel="stylesheet">
-
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
