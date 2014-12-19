e46384fb27a02d2695093500644b740541b996da
Vultor <shawn@shawnroe.com>
2014-12-19 17:16:52 +0900

    Base index for future galleries, just replace the dataTitle and dataDate

---
+++ galleries/index-base.php
@@ -0,0 +1,162 @@
+<? include("../../head.php"); ?>
+<div class="container">
+  <!-- main slider carousel -->
+
+  <div class="row">
+    <div class="col-md-12" id="slider">
+      <div class="col-md-12" id="carousel-bounding-box">
+        <div class="carousel slide" id="testCarousel">
+          <!-- main slider carousel items -->
+
+          <div class="carousel-inner">
+            <div class="item active" data-slide-number="0">
+              <img class="img-responsive" src="michaelanthonysimon166.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="1">
+              <img class="img-responsive" src="michaelanthonysimon167.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="2">
+              <img class="img-responsive" src="michaelanthonysimon168.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="3">
+              <img class="img-responsive" src="michaelanthonysimon169.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="4">
+              <img class="img-responsive" src="michaelanthonysimon170.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="5">
+              <img class="img-responsive" src="michaelanthonysimon171.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="6">
+              <img class="img-responsive" src="michaelanthonysimon167.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="7">
+              <img class="img-responsive" src="michaelanthonysimon168.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="8">
+              <img class="img-responsive" src="michaelanthonysimon169.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="9">
+              <img class="img-responsive" src="michaelanthonysimon170.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="10">
+              <img class="img-responsive" src="michaelanthonysimon171.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="11">
+              <img class="img-responsive" src="michaelanthonysimon171.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+          </div><!--/.carousel-inner -->
+          <a class="carousel-control left" data-slide="prev" href="#testCarousel">‹</a> <a class="carousel-control right" data-slide="next" href="#testCarousel">›</a>
+        </div><!--/#testCarousel-->
+      </div>
+    </div>
+
+    <!-- caption area -->
+    <div class="new-caption-area"></div>
+  </div><!--/.row-->
+  <!-- thumb navigation carousel -->
+
+  <div class="col-md-12 hidden-xs" id="slider-thumbs">
+    <!-- thumb navigation carousel items -->
+    <ul class="list-inline">
+
+      <li>
+        <a class="selected" id="carousel-selector-00"><img class="img-responsive" src="thumbs/michaelanthonysimon166.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-01"><img class="img-responsive" src="thumbs/michaelanthonysimon167.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-02"><img class="img-responsive" src="thumbs/michaelanthonysimon168.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-03"><img class="img-responsive" src="thumbs/michaelanthonysimon169.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-04"><img class="img-responsive" src="thumbs/michaelanthonysimon170.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-05"><img class="img-responsive" src="thumbs/michaelanthonysimon171.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-06"><img class="img-responsive" src="thumbs/michaelanthonysimon167.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-07"><img class="img-responsive" src="thumbs/michaelanthonysimon168.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-08"><img class="img-responsive" src="thumbs/michaelanthonysimon169.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-09"><img class="img-responsive" src="thumbs/michaelanthonysimon170.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-10"><img class="img-responsive" src="thumbs/michaelanthonysimon171.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-11"><img class="img-responsive" src="thumbs/michaelanthonysimon171.jpeg"></a>
+      </li>
+
+    </ul>
+  </div>
+</div>
+<? include("../../foot.php"); ?>
