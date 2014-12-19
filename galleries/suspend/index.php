3aeb1cd8e0b761ea030ece3af2ab50ad5733f9d6
Vultor <shawn@shawnroe.com>
2014-12-19 15:21:34 +0900

    Suspend Gallery

---
+++ galleries/suspend/index.php
@@ -0,0 +1,74 @@
+<? include("../../head.php"); ?>
+<div class="container">
+  <!-- main slider carousel -->
+
+  <div class="row">
+    <div class="col-md-12" id="slider">
+      <div class="col-md-12" id="carousel-bounding-box">
+        <div class="carousel slide" id="testCarousel">
+          <!-- main slider carousel items -->
+
+          <div class="carousel-inner">
+            <div class="item active" data-slide-number="0">
+              <img alt="Heart" class="img-responsive" src="1michaelanthonysimon57.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="1">
+              <img alt="Smile" class="img-responsive" src="1michaelanthonysimon58.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="2">
+              <img class="img-responsive" src="1michaelanthonysimon59.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+            <div class="item" data-slide-number="3">
+              <img class="img-responsive" src="1michaelanthonysimon60.jpeg">
+              <div class="carousel-caption">
+                <span class="image-title">dataTitle</span>dataDate
+              </div>
+            </div>
+
+          </div><!--/.carousel-inner -->
+          <a class="carousel-control left" data-slide="prev" href="#testCarousel">‹</a> <a class="carousel-control right" data-slide="next" href="#testCarousel">›</a>
+        </div><!--/#testCarousel-->
+      </div>
+    </div>
+
+    <!-- caption area -->
+    <div class="new-caption-area"></div>
+  </div><!--/.row-->
+  <!-- thumb navigation carousel -->
+
+  <div class="col-md-12 hidden-xs" id="slider-thumbs">
+    <!-- thumb navigation carousel items -->
+    <ul class="list-inline">
+
+      <li>
+        <a class="selected" id="carousel-selector-00"><img class="img-responsive" src="thumbs/1michaelanthonysimon57.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-01"><img class="img-responsive" src="thumbs/1michaelanthonysimon58.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-02"><img class="img-responsive" src="thumbs/1michaelanthonysimon59.jpeg"></a>
+      </li>
+
+      <li>
+        <a id="carousel-selector-03"><img class="img-responsive" src="thumbs/1michaelanthonysimon60.jpeg"></a>
+      </li>
+
+    </ul>
+  </div>
+</div>
+<? include("../../foot.php"); ?>
